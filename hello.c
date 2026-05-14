3c9cf4cda04ae4f7571a7386b96f735a8e873385
El499846 <el499846@ucf.edu>
2026-05-14 05:26:51 +0000

    2nd edit to understand commit

--- hello.c
+++ hello.c
@@ -1,5 +1,7 @@
 #include <stdio.h> 
 int main(void) {
     printf("Hello, Codespace\n");
+    
+    printf("Adding a new edit to my code to see how to edit the program successfully\n");
     return 0;
 }
